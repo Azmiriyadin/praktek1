4cb9aff3228f5e4b512932fdffcce93abb01b366
Your Name <you@example.com>
2023-09-21 00:43:33 +0700

    praktek 1

---
+++ index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>UAS Desktop Programming</title>
+    <link rel="stylesheet" href="asset/vendors/feather/feather.css">
+    <link rel="stylesheet" href="asset/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="asset/vendors/css/vendor.bundle.base.css">
+    <link rel="stylesheet" href="asset/vendors/ti-icons/css/themify-icons.css">
+    <link rel="stylesheet" href="asset/css/vertical-layout-light/style.css">
+    <script src="asset/js/jquery-3.4.1.min.js"></script>
+    <link rel="shortcut icon" href="main/images/favicon.png">
+</head>
+<body>
+    <div class="container-scroller">
+        <nav class="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+            <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
+                <a class="navbar-brand brand-logo mr-5" href=""><img src="asset/images/logo.svg" class="mr-2" alt="logo"/></a>
+                <a class="navbar-brand brand-logo-mini" href=""><img src="asset/images/logo-mini.svg" alt="logo"/></a>
+            </div>
+            <div class="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+                <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
+                    <span class="icon-menu"></span>
+                </button>
+                <ul class="navbar-nav mr-lg-2">
+                    <li class="nav-item nav-search d-none d-lg-block">
+                        <div class="input-group" style="font-size: 20px;">
+                            Client Side Programming
+                        </div>
+                    </li>
+                </ul>
+                <ul class="navbar-nav mr-lg-2"></ul>
+                <ul class="navbar-nav navbar-nav-right"></ul>
+                <button class="navbar-toggler navbar-toggler-right d-lg-none align-self-center" type="button" data-toggle="offcanvas">
+                    <span class="icon-menu"></span>
+                </button>
+            </div>
+        </nav>
+        <div class="container-fluid page-body-wrapper">
+            <nav class="sidebar sidebar-offcanvas" id="sidebar">
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-grid menu-icon"></i><span class="menu-title">Dashboard</span></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"><i class="icon-grid menu-icon"></i><span class="menu-title">Menu Utama</span></a>
+                    </li>
+                </ul>
+            </nav>
+            <div class="main-panel">
+                <div class="content-wrapper" id="blokkonten"></div>
+                <footer class="footer">
+                  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Copyright © 2021.  Premium <a href="https://www.bootstrapdash.com/" target="_blank">Bootstrap admin template</a> from BootstrapDash. All rights reserved.</span>
+                    <span class="float-none float-sm-right d-block mt-1 mt-sm-0 text-center">Hand-crafted & made with <i class="ti-heart text-danger ml-1"></i></span>
+                  </div>
+                  <div class="d-sm-flex justify-content-center justify-content-sm-between">
+                    <span class="text-muted text-center text-sm-left d-block d-sm-inline-block">Distributed by <a href="https://www.themewagon.com/" target="_blank">Themewagon</a></span> 
+                  </div>
+                </footer> 
+            </div>
+        </div>
+    </div>
+    <script src="asset/js/off-canvas.js"></script>
+    <script src="asset/js/hoverable-collapse.js"></script>
+    <script src="asset/js/template.js"></script>
+    <script src="asset/js/settings.js"></script>
+</body>
+</html>
+
